473f7bebb24399ba608598277854c7a1718e3167
blackwoodbs <65741366+blackwoodbs@users.noreply.github.com>
2021-04-08 16:41:11 -0700

    Update DL_test.py

--- DL_test.py
+++ DL_test.py
@@ -27,7 +27,20 @@ download_dir = "Desktop\Test_SDS"
 cas_file = open('Desktop\mast.csv', 'r') 
 reader = csv.reader(cas_file)
 
-test_cas = "588-63-6" #3-PHENOXYPROPYL BROMIDE
+test_cas = "75-05-8" #3-PHENOXYPROPYL BROMIDE
+
+sigma = "Sigma Aldrich"
+
+x_0 = ["7785-26-4", "(1S)-(-)-Alpha-PINENE"]
+x_1 = ["82509-30-6", "(1R)-(-)-10-CAMPHORSULFONIC ACID AMMONIUM SALT"]
+x_2 = ["17577-28-5", "(ETHOXYCARBONYLMETHYL)TRIPHENYLPHOSPHONIUM CHLORIDE"]
+x_3 = ["50-69-1", "D(-)-RIBOSE"]
+#31598-79-5	2,3,5-TRI-O-BENZYL-1-O-P-NITROBENZOYL-D-ARABINOFURANOSE
+#588-63-6	3-PHENOXYPROPYL BROMIDE
+#637-59-2	1-BROMO-3-PHENYLPROPANE
+#17857-14-6	(3-CARBOXYPROPYL)TRIPHENYLPHOSPHONIUM BROMIDE
+#2530-83-8	(3-GLYCIDYLOXYPROPYL)TRIMETHOXYSILANE
+
 
 driver.get(sds_db)
 
@@ -39,30 +52,36 @@ def downloader(name, wait):
     time.sleep(wait)
     pyautogui.hotkey('enter')
 
-try:
-    element = WebDriverWait(driver, 5).until(
-        EC.presence_of_element_located((By.ID, "cs_txtCas"))
-    )
-    cas_box = driver.find_element_by_id('cs_txtCas').send_keys(test_cas) #cas box
-    supplier_box = driver.find_element_by_id('cs_txtManufact').send_keys("sigma") #manufacturer box
-    search_button = driver.find_element_by_id('cs_btnSearch').click() #search button
-    time.sleep(3)
-    
-    code = driver.page_source
-    soup = BeautifulSoup(code, 'lxml')
-    link_name = soup.find('div', id='cs_divResults').table.a.text
-    print(link_name)
+def lookup(cas, chem, supplier):
+    try:
+        element = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.ID, "cs_txtCas"))
+        )
+        cas_box = driver.find_element_by_id('cs_txtCas').send_keys(cas) #cas box
+        supplier_box = driver.find_element_by_id('cs_txtManufact').send_keys(supplier) #manufacturer box
+        chem_box = driver.find_element_by_id('cs_txtSubstance').send_keys(chem)
+        search_button = driver.find_element_by_id('cs_btnSearch').click() #search button
+        time.sleep(3)
+        
+        code = driver.page_source
+        soup = BeautifulSoup(code, 'lxml')
+        link_name = soup.find('div', id='cs_divResults').table.a.text #sometimes the top result isn't the right options
+        rev = soup.find('div', id='cs_divResults').tbody.tr.td.next.next.next.next.next.next.next.next.next.next #pretty ugly
+        #Finds the date of Revision. must replace '/' with '_' to have it be part of the file name.
+        print(rev)
+        print(link_name)
 
-    link = driver.find_element_by_partial_link_text(link_name).click()
-    time.sleep(5)
-    driver.switch_to.window(driver.window_handles[1])
-    sds = driver.find_element_by_partial_link_text('View SDS').click()
-    time.sleep(3)
+        link = driver.find_element_by_partial_link_text(link_name).click()
+        time.sleep(5)
+        driver.switch_to.window(driver.window_handles[1])
+        sds = driver.find_element_by_partial_link_text('View SDS').click()
+        time.sleep(3)
 
-    downloader(link_name, 2) #THIS WORKS SOMETIMES    .TMP FILE DOWNLOADS SOMETIMES AND I DONT KNOW WHY
-    time.sleep(4)
-except:
-    print('failed')
+        #downloader(cas + "__" + link_name + " SDS", .2) #THIS WORKS SOMETIMES    .TMP FILE DOWNLOADS SOMETIMES AND I DONT KNOW WHY
+        time.sleep(1)
+    except:
+        print('failed')
+
+lookup(x_2[0],x_2[1],sigma)
 
 driver.quit()
-
